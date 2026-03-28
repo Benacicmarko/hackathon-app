c3189653aa518f0f2aaab3aa87af1ff89b9c4ba5
Andre Flego <ade.flego@gmail.com>
2026-03-28 10:56:15 +0100

    project init

---
+++ hackathon-app/hackathon-app/hackathon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathon_appApp.swift
+//  hackathon-app
+//
+//  Created by Andre Flego on 28.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct hackathon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
